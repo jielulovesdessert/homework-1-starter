f37277f8c5cb37096992d531957f165fb4e158a1
jielulovesdessert <31902854+jielulovesdessert@users.noreply.github.com>
2018-01-29 02:27:19 -0500

    Add files via upload

---
+++ task2/test_divide.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 01:11:24 2018
+
+@author: Jie Lu
+"""
+from __future__ import division
+import numpy as np
+
+def test_1():
+    assert 2/8 == 0.25
+
+def test_2():
+    assert np.divide(2,8)==0.25
+    
+
+
+
+    
