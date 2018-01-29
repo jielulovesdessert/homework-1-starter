a273c9c1e4e930f7ca93f1703fd5cff36b125d19
jielulovesdessert <31902854+jielulovesdessert@users.noreply.github.com>
2018-01-29 10:59:14 -0500

    Add files via upload

--- task2/test_text.py
+++ task2/test_text.py
@@ -5,7 +5,9 @@ Created on Mon Jan 29 01:20:45 2018
 @author: Jie Lu
 """
 
+import io
+
 def test():
-    utf8_text=open('task2/input.txt').read()
-    unicode_data = utf8_text.strip().decode('utf8')
-    assert len(unicode_data) == 6
+    text= io.open('task2/input.txt')
+    char=text.read().strip()
+    assert len(char)==6
