07866ab9236a422e45a05a38dcbbe88d0f88fb60
jielulovesdessert <31902854+jielulovesdessert@users.noreply.github.com>
2018-01-29 03:54:29 -0500

    Add files via upload

---
+++ task2/test_text.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 01:20:45 2018
+
+@author: Jie Lu
+"""
+
+def test():
+    utf8_text=open('input.txt').read()
+    unicode_data = utf8_text.strip().decode('utf8')
+    assert len(unicode_data) == 6
