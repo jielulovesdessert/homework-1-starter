5601fd81dffb1a50937a7aa3e0dc03cb2c2e199e
jielulovesdessert <31902854+jielulovesdessert@users.noreply.github.com>
2018-01-29 10:41:56 -0500

    Add files via upload

--- task2/test_text.py
+++ task2/test_text.py
@@ -6,6 +6,6 @@ Created on Mon Jan 29 01:20:45 2018
 """
 
 def test():
-    utf8_text=open('input.txt').read()
+    utf8_text=open('task2/input.txt').read()
     unicode_data = utf8_text.strip().decode('utf8')
     assert len(unicode_data) == 6
